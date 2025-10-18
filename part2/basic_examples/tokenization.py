67e698f5aef477442332e354cb6712ae91183ceb
Jacob Orshalick <jacob@focus.dev>
2025-10-18 16:47:37 -0500

    lock down versions and cleanup script

--- part2/basic_examples/tokenization.py
+++ part2/basic_examples/tokenization.py
@@ -1,16 +1,18 @@
 import tiktoken
 
-def count_tokens(text: str, model_name: str) -> None:
+
+def count_tokens(text: str, model_name: str):
   # all OpenAI models are supported
   encoding = tiktoken.encoding_for_model(model_name)
-  
+
   # Tokenize the text
   tokens = encoding.encode(text)
-  
+
   print(tokens)
   print(f"Number of tokens: {len(tokens)}")
 
+
 model_name = "gpt-4o-mini"
-sample_text = "I'm having password issues, can you help me login?"
+sample_text = "I'm having password issues, can you help me log in?"
 
 count_tokens(sample_text, model_name)
