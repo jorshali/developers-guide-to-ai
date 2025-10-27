4ae8ff57d7167e778647be71aa615f5aa7ce58ed
Jacob Orshalick <jacob@focus.dev>
2025-10-26 19:18:25 -0500

    move summarization prompt messages into templates for exmaple

--- part2/conversation_history/summarize_history.py
+++ part2/conversation_history/summarize_history.py
@@ -1,9 +1,51 @@
 import json
 
 from ollama import chat
+from jinja2 import Environment, FileSystemLoader
 
 from conversation_history import ConversationHistory
 from conversation_history_example_data import conversation_history
+
+env = Environment(
+  loader=FileSystemLoader("templates")
+)
+
+system_template = env.get_template('summarize_history_system_prompt.txt')
+user_template = env.get_template('summarize_history_system_prompt.txt')
+
+
+def summarize_history(conversation_history: ConversationHistory, max_tokens=120):
+  system_message = system_template.render(
+    max_summary_tokens=max_tokens)
+
+  user_message = user_template.render(
+    message_history_as_string=conversation_history.message_history_as_string()
+  )
+
+  response = chat(
+    model="llama3.2",
+    messages=[
+      {
+        "role": "system",
+        "content": system_message
+      },
+      {
+        "role": "user",
+        "content": user_message
+      }
+    ],
+    stream=False,
+    options={
+      'num_predict': max_tokens,
+      'temperature': 0
+    }
+  )
+
+  conversation_history.message_history = [{
+    'role': 'user',
+    'content': response.message.content
+  }]
+
 
 history = ConversationHistory(
   {'role': 'system', 'content': """You are a helpful Help Desk Representative. Use the following 
